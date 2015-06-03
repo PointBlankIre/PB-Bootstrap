535e716713c0300eab1ff053c2a38a5475a32da4
urlough <turlough@pointblank.ie>
2015-06-03 16:49:24 +0100

    Update functions.php
    Bootstrap Pagination

--- functions.php
+++ functions.php
@@ -115,3 +115,29 @@ function add_cookie_warning()
 }
 add_action( 'wp_enqueue_scripts', 'add_cookie_warning' );
 
+
+// Boostrap Pagination
+        
+function custom_pagination() {
+    global $wp_query;
+    $big = 999999999; // need an unlikely integer
+    $pages = paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages,
+            'type'  => 'array',
+            'prev_next'   => true,
+      'prev_text'    => __('«'),
+      'next_text'    => __('»'),
+        ) );
+        if( is_array( $pages ) ) {
+            $paged = ( get_query_var('paged') == 0 ) ? 1 : get_query_var('paged');
+            echo '<ul class="pagination">';
+            foreach ( $pages as $page ) {
+                    echo "<li>$page</li>";
+            }
+           echo '</ul>';
+        }
+}
+
