4bbe001c58d1c4d41ecb325a9f8968398186219d
Dom Muldoon <dom@pointblank.ie>
2015-06-04 17:04:19 +0100

    test add

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	
-	<h1><?php the_title(); ?></h1>
+	<h1><?php the_title(); ?> added by git push</h1>
 
 	
 	  	<?php the_content(); ?>
