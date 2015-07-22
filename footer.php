1a125f11e02b81aa96cb0372554b1f5a41bc4a2d
dommuldoon <dom@pointblank.ie>
2015-07-22 15:33:16 +0100

    Update footer.php
    added code for new cookie warning function call with options

--- footer.php
+++ footer.php
@@ -57,7 +57,23 @@
 
 	jQuery(function() { 
 
-
+		jQuery.cookiesDirective({
+	            explicitConsent: true, // false allows implied consent
+	            position: 'bottom', // top or bottom of viewport
+	            duration: 10, // display time in seconds
+	            limit: 0, // limit disclosure appearances, 0 is forever     
+	            message: null, // customise the disclosure message              
+	            cookieScripts: null, // disclose cookie settings scripts
+	            privacyPolicyUri: '/privacy-statement/',   // uri of your privacy policy
+	            scriptWrapper: function(){}, // wrapper function for cookie setting scripts
+	            fontFamily: 'helvetica', // font style for disclosure panel
+	            fontColor: '#FFFFFF', // font color for disclosure panel
+	            fontSize: '14px', // font size for disclosure panel
+	            backgroundColor: '#000000', // background color of disclosure panel
+	            backgroundOpacity: '60', // opacity of disclosure panel
+	            linkColor: '#CCCCCC' // link color in disclosure panel
+	        });
+        
 			jQuery('.sidebar .dropdown-menu a').click(function(e) {
 			    e.stopPropagation();
 			});
